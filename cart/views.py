05e08b76a2fc2aa42acea2bddcb05b4a3cdb434b
amirvalie <hosseinvalie@gmail.com>
2022-11-21 01:47:28 -0500

    added cart_remove view

--- cart/views.py
+++ cart/views.py
@@ -29,4 +29,10 @@ def cart_add(request, product_id):
         form = CartAddProductFrom()
         return HttpResponse("form is not valid ")
 
+@require_POST
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart:cart_detail')
 
