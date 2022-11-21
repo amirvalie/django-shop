8487f7c5050517c6918b0807b609247d9e739165
amirvalie <hosseinvalie@gmail.com>
2022-11-21 01:52:22 -0500

    added coupon_apply view

--- coupon/views.py
+++ coupon/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .forms import CouponApplyForm
+from .models import Coupon
+from django.shortcuts import redirect
+from django.views.decorators.http import require_POST
+from django.views.generic.edit import FormView
+from django.http import HttpResponse
 # Create your views here.
+
+
+@require_POST
+def coupon_apply(request):
+    now = timezone.now()
+    if request.method == 'POST':
+        form = CouponApplyForm(request.POST)
+        if form.is_valid():
+            code = form.cleaned_data['code']
+            try:
+                coupon = Coupon.objects.get(code__iexact=code,
+                valid_from__lte=now,
+                valid_to__gte=now,
+                active=True)
+                request.session['coupon_id'] = coupon.id
+            except Coupon.DoesNotExist:
+                request.session['coupon_id'] = None
+        return redirect('orders:list_address')
+    else:
+        return HttpResponse("Invalid request")
