253c48e12600a28ba307acf4d57eb50aaf081369
amirvalie <hosseinvalie@gmail.com>
2023-06-04 12:44:52 +0330

    Added categories models to the admin file

--- django_shop/categories/admin.py
+++ django_shop/categories/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import (
+    Category,
+    Banner,
+)
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'parent']
+    list_filter = ['title']
+    search_fields = ['title', 'slug', 'description']
+
+
+class BannerAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+    list_filter = ['title']
+    search_fields = ['title', 'slug', 'description']
+
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Banner,BannerAdmin)
