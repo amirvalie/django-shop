e0d19a2a777527c79ea2e85ae2ab94f9d7e76482
amirvalie <hosseinvalie@gmail.com>
2023-06-05 22:23:05 +0330

    fixed name:special_offer selector name changed to special_offer_category

--- django_shop/products/blogic/selectors.py
+++ django_shop/products/blogic/selectors.py
@@ -26,7 +26,7 @@ def session_discount() -> Iterable[Banner]:
     )
 
 
-def special_offer() -> Iterable[Banner]:
+def special_offer_category() -> Iterable[Banner]:
     return Banner.objects.filter(
         status='p',
         type_category='special_offer'
@@ -58,6 +58,6 @@ def category_products(slug:str) ->Iterable[Product]:
     return category.category_products.product_publish()
 
 
-def special_offer(slug:str) ->Iterable[Product]:
+def special_offers(slug:str) ->Iterable[Product]:
     banner=get_banner(slug)
     return banner.banner_products.product_publish()
