b66ab7124c58baceb1626b2cec5b08e255cf7550
amirvalie <hosseinvalie@gmail.com>
2023-06-28 11:45:48 +0330

    Changed swagger path

--- config/urls.py
+++ config/urls.py
@@ -10,8 +10,8 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path("schema/", SpectacularAPIView.as_view(api_version="v1"), name="schema"),
-    path("", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("api/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
     path('admin/', admin.site.urls),
-    path('api/', include(('django_shop.api.urls', 'api'))),
+    path('', include(('django_shop.api.urls', 'api'))),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
