60bdd5b9216eb85115c80d1ee2d24720c26b1216
amirvalie <hosseinvalie@gmail.com>
2023-06-28 14:27:29 +0330

    Added coupon urls to the api app urls

--- django_shop/api/urls.py
+++ django_shop/api/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include(('django_shop.products.urls', 'product'))),
     path('', include(('django_shop.django_phone_login.urls', 'django_phone_login'))),
+    path('', include(('django_shop.coupon.urls', 'coupon'))),
 ]
