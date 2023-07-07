3500f7b360f7c00b506a822fcc62cf6f460b277c
amirvalie <hosseinvalie@gmail.com>
2023-07-07 16:00:17 +0330

    Deleted django_phone_login module

--- django_shop/django_phone_login/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DjangoPhoneLoginConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django_shop.django_phone_login'
