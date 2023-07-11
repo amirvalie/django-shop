ef1b1cfe1bcbbc68fef6e6ee6e11fc0882357ca3
amirvalie <hosseinvalie@gmail.com>
2023-07-11 16:14:24 +0330

    Adde ProductSerializer and CategorySerializer

--- django_shop/products/api/serializers.py
+++ django_shop/products/api/serializers.py
@@ -1,8 +1,40 @@
 from rest_framework import serializers
 from ..models import Product
+from ...categories.models import Category
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(queryset=User.objects.all(), slug_field='phone')
+    category = serializers.SlugRelatedField(many=True, queryset=Category.objects.all(), slug_field='slug')
+
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = [
+            'user',
+            'title',
+            'slug',
+            'category',
+            'banner',
+            'brand',
+            'introduction',
+            'price',
+            'discount_product',
+            'thumbnail',
+            'quantity',
+            'available',
+            'sales_number',
+        ]
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = [
+            'title',
+            'slug',
+            'status',
+            'thumbnail',
+            'parent',
+        ]
