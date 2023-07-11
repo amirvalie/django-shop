5426f7c2e89c7498160ab975da1034bb8bc13ea6
amirvalie <hosseinvalie@gmail.com>
2023-07-11 16:21:41 +0330

    Rmoved * at the beginning of functions

--- django_shop/products/blogic/selectors.py
+++ django_shop/products/blogic/selectors.py
@@ -5,9 +5,9 @@ from ..models import (
     Product,
     IpAddress,
     Brand,
-    Banner,
 )
-from django_shop.categories.blogic.selectors import get_category, get_banner
+from ...categories.models import Banner
+from ...categories.blogic.selectors import get_category, get_banner
 from typing import Iterable, Any
 from rest_framework.response import Response
 from django_filters import rest_framework as filters
@@ -19,7 +19,7 @@ class ProudctFilter(filters.FilterSet):
         fields = ['title']
 
 
-def product_list(*, filters=None) -> Iterable[Product]:
+def product_list(filters=None) -> Iterable[Product]:
     if filters:
         filters = filters or {}
         products = Product.objects.product_publish()
@@ -53,24 +53,24 @@ def get_product(**kwargs) -> Product:
     return get_object_or_404(Product, **kwargs)
 
 
-def related_product_list(*, product: Product) -> Iterable[Product]:
+def related_product_list(product: Product) -> Iterable[Product]:
     return Product.objects.filter(
         title__icontains=product.title[:5]
     ).exclude(id=product.id)
 
 
-def product_search(*, query: str) -> Iterable[Product]:
+def product_search(query: str) -> Iterable[Product]:
     return Product.objects.filter(Q(title__icontain=query) |
                                   Q(slug__icontain=query)
                                   )
 
 
-def category_products(*, slug: str) -> Iterable[Product]:
+def category_products(slug: str) -> Iterable[Product]:
     category = get_category(slug)
     return category.category_products.product_publish()
 
 
-def special_offers(*, slug: str) -> Iterable[Product]:
+def special_offers(slug: str) -> Iterable[Product]:
     banner = get_banner(slug)
     return banner.banner_products.product_publish()
 
@@ -83,16 +83,16 @@ def best_selling_products() -> Iterable[Product]:
     return Product.objects.product_sales().order_by('-max_sales_number')
 
 
-def get_brand(*, brand_name: str) -> Brand:
+def get_brand(brand_name: str) -> Brand:
     return get_object_or_404(Brand, brand_name=brand_name)
 
 
-def brand_products(*, brand_name: str) -> Iterable[Product]:
+def brand_products(brand_name: str) -> Iterable[Product]:
     brand = get_brand(brand_name)
     return brand.products.product_publish()
 
 
-def get_response_paginator(*, paginator_class, queryset, serializer_class, request, view) -> Response | Any:
+def get_response_paginator(paginator_class, queryset, serializer_class, request, view) -> Response | Any:
     paginator = paginator_class()
     page = paginator.paginate_queryset(queryset, request, view=view)
     if page is not None:
