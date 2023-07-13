fd5637822350872294a7781c36ae8b400313e8c9
amirvalie <hosseinvalie@gmail.com>
2023-07-13 12:19:20 +0330

    Add feature:limiting access of the noraml users to user,discounted_product,quantity,sales_number fields

--- django_shop/products/api/serializers.py
+++ django_shop/products/api/serializers.py
@@ -1,13 +1,24 @@
-from rest_framework import serializers
+from rest_framework import serializers, permissions
 from ..models import Product
 from ...categories.models import Category
 from django.contrib.auth import get_user_model
+
 User = get_user_model()
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    def __init__(self, *args, **kwargs):
+        # Instantiate the superclass normally
+        super(ProductSerializer, self).__init__(*args, **kwargs)
+        request = kwargs.get('context').get('request')
+        if not request.user.is_staff:
+            not_allowed_fields = ['user', 'discount_product', 'quantity', 'sales_number']
+            for not_allowed_field in not_allowed_fields:
+                self.fields.pop(not_allowed_field)
+
     user = serializers.SlugRelatedField(queryset=User.objects.all(), slug_field='phone')
     category = serializers.SlugRelatedField(many=True, queryset=Category.objects.all(), slug_field='slug')
+    discount_product = serializers.CharField()
 
     class Meta:
         model = Product
